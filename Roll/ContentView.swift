857337875e17d5feaf532ab927de0f804da81ce0
Carolina Ortega <cahortega@outlook.com>
2022-06-07 14:05:46 -0300

    create arView, start session and create overlay

--- Roll/ContentView.swift
+++ Roll/ContentView.swift
@@ -7,30 +7,38 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
-struct ContentView : View {
+struct ContentView: View {
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        return ARViewContainer().ignoresSafeArea()
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
+        let arView = ARView()
         
-        let arView = ARView(frame: .zero)
+        //Começar a sessão
+        let session = arView.session
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        session.run(config)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        //adicionando overlay
+        let overlay = ARCoachingOverlayView()
+        overlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        overlay.session = session
+        overlay.goal = .horizontalPlane
+        arView.addSubview(overlay)
         
         return arView
         
     }
-    
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        
+    }
     
 }
 
